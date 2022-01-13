e80e8ebc53befa07ccca06652f6d5ebdad30b4c4
Glecia <gleciaparis@gmail.com>
2022-01-13 12:37:36 +0100

    initial commit

--- JavaScript/table.js
+++ JavaScript/table.js
@@ -17,7 +17,7 @@ async function render(){
         <th scope="row">${country.name.official}</td>
         <td class="text-end">${FORMATER.format(country.area)}</td>
         <td class="text-end">${FORMATER.format(country.population)}</td>
-        <td>${country.capital[country.capital.length-1]}</td></tr>`;
+        <td>${country.capital[0]}</td></tr>`;
     }
 
     let tbody=document.querySelector("#countriesTable");
@@ -27,3 +27,4 @@ async function render(){
 
  
 window.addEventListener('load',render());
+
