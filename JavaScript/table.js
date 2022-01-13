4a1182a1b502009a73d7053a238b8aea7a14cf26
Glecia <gleciaparis@gmail.com>
2022-01-13 09:39:33 +0100

    initial commit

---
+++ JavaScript/table.js
@@ -0,0 +1,29 @@
+
+
+
+async function render(){
+   
+    const response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,area,population,capital');
+    
+    
+    const countries= await response.json();
+
+    const FORMATER = new Intl.NumberFormat('en-US');
+    
+     let table='';
+
+    for (const country of countries){
+        table+=`<tr>
+        <th scope="row">${country.name.official}</td>
+        <td class="text-end">${FORMATER.format(country.area)}</td>
+        <td class="text-end">${FORMATER.format(country.population)}</td>
+        <td>${country.capital[country.capital.length-1]}</td></tr>`;
+    }
+
+    let tbody=document.querySelector("#countriesTable");
+    tbody.innerHTML=table;
+
+}
+
+ 
+window.addEventListener('load',render());
